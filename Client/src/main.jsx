50bf8fa5b365275318386297d2423c4d8b570dc2
supriya-patil-17 <spatil54611@gmail.com>
2025-07-22 12:28:19 +0530

    Initial import

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,9 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Assembly from './Components/Assembly.jsx'
+import Variables from './Components/Variables.jsx'
 
 createRoot(document.getElementById('root')).render(
 
-    <Assembly/>
-  
+    <Assembly />
+
+)
+
+createRoot(document.getElementById('app')).render(
+    <Variables />
+
 )
