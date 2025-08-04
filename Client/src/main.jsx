91cef4f32a41a368605987a5fe3742d3123eba0b
supriya.patil <supriya-patil-17>
2025-08-04 14:35:08 +0530

    initial commit

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,13 +1,11 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Assembly from './Components/Assembly.jsx'
-import Variables from './Components/Variables.jsx'
-import Mechanism from './Components/Mechanism.jsx'
+
 
 createRoot(document.getElementById('root')).render(
 
-    <Mechanism />
+    <App />
 
 )
